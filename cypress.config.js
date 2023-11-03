77aca96ecc285737a86e48fe86211f1e9375281d
KyeongSeopHam <rudtjq5724@gmail.com>
2023-11-03 15:04:50 +0900

    gitignore add

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,8 @@ module.exports = defineConfig({
   projectId: 'sjwo6q',
   e2e: {
     baseUrl: 'https://accounts.elice.io/',
-    supportFile: false
-    },
+    supportFile: false,
+    video: true 
+  },
   },
 );
