7e2d40eea3e892a7b2b07fd8a6f1a2ff37888846
KyeongSeopHam <rudtjq5724@gmail.com>
2023-11-03 20:02:29 +0900

    config baseUrl add

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'sjwo6q',
   e2e: {
-    baseUrl: 'https://accounts.elice.io/',
-    supportFile: false,
-    video: true 
+    baseUrl: 'https://accounts.elice.io/',  
   },
-  },
-);
+});
