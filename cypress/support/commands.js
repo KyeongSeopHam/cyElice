c48b3fbed749fd93c79fb17598ed1adc2c3a6155
KyeongSeopHam <rudtjq5724@gmail.com>
2023-11-05 17:14:19 +0900

    cypress ci End

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,8 +26,9 @@ Cypress.Commands.add('checkPageLoadAndComponents', () => {
   // cy.contains('비밀번호를 잊어버리셨나요?').click();
   cy.get(".MuiStack-root > .MuiTypography-root").click(); // 비밀번호를 잊어버리셨나요?
 
-  cy.url().should('include', '/accounts/recover/password/find/email?continue_to=https%3A%2F%2Faccounts.elice.io%2F&lang=ko');  // en(깃액션) -> ko(cypress) -> en 
-  //cy.url().should('eq', '/accounts/recover/password/find/email?continue_to=https%3A%2F%2Faccounts.elice.io%2F&lang=ko');
+  //cy.url().should('include', '/accounts/recover/password/find/email?continue_to=https%3A%2F%2Faccounts.elice.io%2F&lang=ko');  // en(깃액션) -> ko(cypress) -> en 
+  cy.url().should('include', '/accounts/recover/password/find/email').should('include', 'lang=ko');
+
 
 
   cy.visit('/')
